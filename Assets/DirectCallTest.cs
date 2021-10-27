c7cf2188974242f08348feec5fdaaeab00f60218
Keijiro Takahashi <keijiro@gmail.com>
2021-10-27 11:47:44 +0900

    Rename: NoiseTexture -> DirectCallTest

--- Assets/DirectCallTest.cs
+++ Assets/DirectCallTest.cs
@@ -4,7 +4,7 @@ using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 
-sealed class NoiseTexture : MonoBehaviour
+sealed class DirectCallTest : MonoBehaviour
 {
     [SerializeField] int _size = 256;
     [SerializeField] RawImage _target = null;
