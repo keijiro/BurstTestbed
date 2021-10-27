2a25a0c86f7ddd9e8a2b16fdecbfef47deaa6b24
Keijiro Takahashi <keijiro@gmail.com>
2021-10-27 11:44:08 +0900

    Update SpanTest

--- Assets/SpanTest.cs
+++ Assets/SpanTest.cs
@@ -1,26 +1,52 @@
 using Unity.Burst;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 sealed class SpanTest : MonoBehaviour
 {
+    [SerializeField] int _size = 256;
+    [SerializeField] RawImage _target = null;
+
+    Texture2D _texture;
+
     void Start()
-      => Debug.Log(SpanTestRunner.Run());
+      => _target.texture = _texture =
+           new Texture2D(_size, _size, TextureFormat.RGBA32, false);
+
+    void Update()
+    {
+        using var buffer = new NativeArray<uint>(_size * _size, Allocator.Temp);
+        TextureGenerator2.Update(buffer, _size, Time.time);
+        _texture.LoadRawTextureData(buffer);
+        _texture.Apply();
+    }
 }
 
 [BurstCompile]
-static class SpanTestRunner
+static class TextureGenerator2
 {
+    unsafe public static void Update(NativeArray<uint> buffer, int size, float time)
+      => Update_Burst(NativeArrayUnsafeUtility.GetUnsafePtr(buffer), size, time);
+
     [BurstCompile]
-    public static float Run()
+    unsafe public static void Update_Burst(void* pointer, int size, float time)
     {
-        var span = (Span<float>)(stackalloc float[10]);
-        FillSpan(span);
-        return span[span.Length - 1];
+        var buffer = new Span<uint>(pointer, size * size);
+        var offs = 0;
+        for (var y = 0; y < size; y++)
+            for (var x = 0; x < size; x++)
+                buffer[offs++] = GetPixel(x, y, time);
     }
 
-    static void FillSpan(Span<float> span)
+    static uint GetPixel(int x, int y, float t)
     {
-        for (var i = 0; i < span.Length; i++) span[i] = 0.1f * i;
+        var pos = math.float3(x, y, t) * math.float3(0.008f, 0.008f, 0.5f);
+        var f32 = noise.snoise(pos) * 0.4f + 0.5f;
+        var un8 = (uint)(math.saturate(f32) * 255);
+        return un8 | un8 << 8 | un8 << 16 | 0xff000000;
     }
 }
