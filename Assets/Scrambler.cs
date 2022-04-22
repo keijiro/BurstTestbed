f00450afcec8b19405445214fb1eb8b370f636c7
Keijiro Takahashi <keijiro@gmail.com>
2022-04-22 14:07:40 +0900

    Use Profiler instead of Stopwatch

--- Assets/Scrambler.cs
+++ Assets/Scrambler.cs
@@ -2,22 +2,20 @@ using System;
 using System.Runtime.InteropServices;
 using Unity.Burst;
 using UnityEngine;
+using UnityEngine.Profiling;
 using UnityEngine.UI;
-using Stopwatch = System.Diagnostics.Stopwatch;
 
 [BurstCompile]
 sealed class Scrambler : MonoBehaviour
 {
     [SerializeField] Texture2D _source = null;
     [SerializeField] RawImage[] _imageViews = null;
-    [SerializeField] Text _label = null;
 
     const uint Key = 0xdeafbeef;
 
     unsafe void Start()
     {
         var buffer = _source.GetPixels32();
-        var sw = new Stopwatch();
 
         fixed (uint* ptr = MemoryMarshal.Cast<Color32, uint>(buffer))
         {
@@ -25,7 +23,9 @@ sealed class Scrambler : MonoBehaviour
             var scrambled = new Texture2D(_source.width, _source.height);
             _imageViews[1].texture = scrambled;
 
-            sw.Start(); Scramble_Burst(ptr, buffer.Length, Key); sw.Stop();
+            Profiler.BeginSample("Scrambling");
+            Scramble_Burst(ptr, buffer.Length, Key);
+            Profiler.EndSample();
 
             scrambled.SetPixels32(buffer);
             scrambled.Apply();
@@ -34,14 +34,15 @@ sealed class Scrambler : MonoBehaviour
             var unscrambled = new Texture2D(_source.width, _source.height);
             _imageViews[2].texture = unscrambled;
 
+            Profiler.BeginSample("Unscrambling");
             Scramble_Burst(ptr, buffer.Length, Key);
+            Profiler.EndSample();
 
             unscrambled.SetPixels32(buffer);
             unscrambled.Apply();
         }
 
         _imageViews[0].texture = _source;
-        _label.text = $"Processing time: {sw.Elapsed.TotalMilliseconds} ms";
     }
 
     [BurstCompile]
